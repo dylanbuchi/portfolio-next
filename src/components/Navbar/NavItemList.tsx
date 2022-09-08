3577d8f1307730be9f4070d06b8cbfb29c1424cf
Dylan Buchi <devybuchi@gmail.com>
2022-09-08 17:24:46 -0300

    Add transition style to nav items

--- src/components/Navbar/NavItemList.tsx
+++ src/components/Navbar/NavItemList.tsx
@@ -22,7 +22,7 @@ export default function NavItemList({
         key={name}
         name={name}
         handleActiveNavbarItem={() => handleActiveNavbarItem(name)}
-        className={`px-3 py-2 rounded-md text-sm font-medium ${
+        className={`transition duration-100 px-3 py-2 rounded-md text-sm font-medium ${
           name === activeNavbarItem ? navItemActiveStyle : ''
         }`}
       />
