2611d8ac21ed0f725c62e847baff2a9ab0d242f0
Dylan Buchi <devybuchi@gmail.com>
2022-09-08 16:17:51 -0300

    Move navItemNames and navItemActiveStyle

---
+++ src/global/Navbar/navbar-items.ts
@@ -0,0 +1,10 @@
+export const navItemNames = new Set<string>([
+  'Home',
+  'Projects',
+  'Certificates',
+  'About me',
+  'Contact',
+]);
+
+export const navItemActiveStyle =
+  'bg-gray-900 text-gray-100 cursor-default pointer-events-none';
