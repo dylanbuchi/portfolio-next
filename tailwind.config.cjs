63ccc0aeed930d264e19a779a3f8472a1857d466
Dylan Buchi <devybuchi@gmail.com>
2022-09-09 15:25:56 -0300

    Add 404 page background image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,9 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      backgroundImage: {
+        'page-404': "url('./src/assets/images/pages/404/404-background.jpg')",
+      },
       fontFamily: {
         pacifico: ['Pacifico, cursive'],
         inter: ['Inter'],
