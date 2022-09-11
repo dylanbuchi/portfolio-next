887abbc920c0864caa3983ef519c32add71447d8
Dylan Buchi <devybuchi@gmail.com>
2022-09-11 08:55:22 -0300

    Add SocialLink component

---
+++ src/components/react/SocialLink.tsx
@@ -0,0 +1,25 @@
+import type { ReactElement } from 'react';
+import type { IconType } from 'react-icons';
+
+interface SocialLinkProps {
+  renderIcon: () => ReactElement<IconType>;
+  href: string;
+  classes?: string;
+}
+
+export default function SocialLink({
+  renderIcon,
+  href = '/#',
+  classes = '',
+}: SocialLinkProps) {
+  return (
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
+      className={`bg-[#bed6d6] dark:bg-[#1c5e5e] shadow-sm dark:shadow-none shadow-greyDarker p-3 rounded text-3xl cursor-pointer hover:shadow-none hover:bg-white transition hover:scale-150 dark:shadow-white dark:hover:bg-gray-900 ${classes}`}
+    >
+      {renderIcon()}
+    </a>
+  );
+}
