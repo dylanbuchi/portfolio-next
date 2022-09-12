7a05ea514ad86b8685b03e142ac4efd972c9ad56
Dylan Buchi <devybuchi@gmail.com>
2022-09-12 17:45:39 -0300

    Refactor and update config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  server: { port: 4000 },
   integrations: [
     react(),
     tailwind({
--- src/components/react/Navbar/NavItemList.tsx
+++ src/components/react/Navbar/NavItemList.tsx
@@ -28,7 +28,9 @@ export default function NavItemList({
         name={name}
         handleActiveNavbarItem={() => handleActiveNavbarItem(name)}
         className={`transition duration-100 px-3 py-2 rounded-md text-sm font-medium ${
-          name === activeNavbarItem ? navItemActiveStyle : ''
+          name.toLocaleLowerCase() === activeNavbarItem.toLocaleLowerCase()
+            ? navItemActiveStyle
+            : ''
         }`}
       />
     ));
