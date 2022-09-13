9ed6d2d5ccfd36b7253ca4e3fad9e02a3e1714df
Dylan Buchi <devybuchi@gmail.com>
2022-09-13 11:40:12 -0300

    Refactor NavItemList

--- src/components/Navbar/NavItemList.tsx
+++ src/components/Navbar/NavItemList.tsx
@@ -1,7 +1,7 @@
 import NavItem from './NavItem';
 
 interface NavItemProps {
-  handleActiveNavbarItem: (name: string) => void;
+  handleActiveNavbarItem: (route: string) => void;
   isMobile?: boolean;
   activeNavbarItem: string;
 }
@@ -11,36 +11,34 @@ const NavItemList = ({
   activeNavbarItem,
   isMobile = false,
 }: NavItemProps) => {
-  const navItemNames = new Set<string>([
-    'Home',
-    'About me',
-    'Projects',
-    'Certificates',
-    'Contact',
+  const navItemNamesAndRoutes = new Set<string[]>([
+    ['Home', '/'],
+    ['About me', '/about-me'],
+    ['Projects', '/projects'],
+    ['Certificates', '/certificates'],
+    ['Contact', '/contact'],
   ]);
 
   const navItemActiveStyle =
     'dark:bg-greyDarker bg-teal-700 text-whiter cursor-default pointer-events-none';
 
   const display = () =>
-    [...Array.from(navItemNames)].map((name) => (
-      <NavItem
-        href={
-          name === 'Home'
-            ? '/'
-            : `/${name.trim().toLocaleLowerCase().replace(' ', '-')}`
-        }
-        isMobile={isMobile}
-        key={name}
-        name={name}
-        handleActiveNavbarItem={() => handleActiveNavbarItem(name)}
-        className={`transition duration-100 px-3 py-2 rounded-md text-sm font-medium ${
-          name.toLocaleLowerCase() === activeNavbarItem.toLocaleLowerCase()
-            ? navItemActiveStyle
-            : ''
-        }`}
-      />
-    ));
+    Array.from(navItemNamesAndRoutes).map(([name, route]) => {
+      return (
+        <NavItem
+          href={route}
+          isMobile={isMobile}
+          key={route}
+          name={name}
+          handleActiveNavbarItem={() => handleActiveNavbarItem(route)}
+          className={`transition duration-100 px-3 py-2 rounded-md text-sm font-medium ${
+            route === activeNavbarItem.toLocaleLowerCase()
+              ? navItemActiveStyle
+              : ''
+          }`}
+        />
+      );
+    });
 
   return <>{display()}</>;
 };
