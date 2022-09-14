74222490ce14c3fb375f62d7c9948d4dc3984628
Dylan Buchi <devybuchi@gmail.com>
2022-09-14 05:22:56 -0300

    Add isUndefined function

--- src/utilities/functions.ts
+++ src/utilities/functions.ts
@@ -1,4 +1,8 @@
 const setClassNames = (...classes: string[]) =>
   classes.filter(Boolean).join(' ');
 
+export const isUndefined = (value: unknown) => {
+  return typeof value === 'undefined';
+};
+
 export default setClassNames;
