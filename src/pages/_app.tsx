8fd7db9fec88ae925e9855376cbcaaac4b765452
Dylan Buchi <devybuchi@gmail.com>
2022-09-15 05:16:09 -0300

    Update app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Footer from 'components/Footer';
 import NavBar from 'components/Navbar/Navbar';
 import type { AppProps } from 'next/app';
 import useIsComponentLoaded from 'utilities/hooks/useIsComponentLoaded';
@@ -8,10 +9,11 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     isLoaded && (
-      <>
+      <main className="flex min-h-[100vh] min-w-[100vw] flex-col bg-home bg-cover text-gray_10 dark:bg-primary_10 dark:bg-home_dark dark:text-white_gray">
         <NavBar />
         <Component {...pageProps} />
-      </>
+        <Footer />
+      </main>
     )
   );
 };
