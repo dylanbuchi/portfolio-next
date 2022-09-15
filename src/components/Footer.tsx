467ebbc337cd4783be21aa00bd90c9649df94abd
Dylan Buchi <devybuchi@gmail.com>
2022-09-15 05:05:46 -0300

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className="mt-auto w-[100%] bg-primary_20 dark:bg-primary_10 ">
+      <div className="pt-12">
+        <div className="min-w[375px] flex max-w-[1945px] items-end justify-center">
+          <p className="mb-4 text-center text-xs text-gray-300 ">
+            &copy; 2019 - 2022 <span className="ml-2">Dylan Buchi</span>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
