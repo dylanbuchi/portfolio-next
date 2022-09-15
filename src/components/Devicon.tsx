5fa0a8f1cbca2c68b7bf4bffbb70a0dc7b6bfd09
Dylan Buchi <devybuchi@gmail.com>
2022-09-15 04:59:11 -0300

    Add Devicon component

---
+++ src/components/Devicon.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+
+type DeviconType = 'original' | 'plain' | 'line';
+
+interface DeviconProps {
+  name: string;
+  type?: DeviconType;
+  darkMode?: boolean;
+  className?: string;
+}
+
+export const Devicon = ({
+  name,
+  type = 'original',
+  className = '',
+}: DeviconProps) => {
+  const filterName = name.toLowerCase().trim();
+
+  return (
+    <Image
+      className={className}
+      alt={name}
+      layout={'fill'}
+      src={`/assets/images/icons/${filterName}/${filterName}-${type}.svg`}
+    ></Image>
+  );
+};
