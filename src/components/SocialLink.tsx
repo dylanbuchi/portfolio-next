83eced49d9b7eb57c8da5b308ad2cf71f9327a29
Dylan Buchi <devybuchi@gmail.com>
2022-09-15 05:17:35 -0300

    Update SocialLink styles

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -17,7 +17,7 @@ const SocialLink = ({
       target="_blank"
       rel="noopener noreferrer"
       href={href}
-      className={`bg-[#dae6f2] dark:bg-[#254d77] shadow-sm dark:shadow-none shadow-primary_10 p-3 rounded sm:text-3xl cursor-pointer hover:shadow-none hover:bg-transparent dark:hover:bg-transparent transition hover:scale-150 ${classes} mr-8 text-xl sm:m-0`}
+      className={`cursor-pointer rounded bg-[#dae6f2] p-3 shadow-sm shadow-primary_10 transition hover:scale-150 hover:bg-transparent hover:shadow-none dark:bg-[#254d77] dark:shadow-none dark:hover:bg-transparent sm:text-3xl ${classes} mr-8 text-xl sm:m-0 `}
     >
       {renderIcon()}
     </a>
