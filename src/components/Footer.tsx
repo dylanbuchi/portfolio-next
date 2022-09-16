fda3722721d938d2222e471a8a8e349fe9069470
Dylan Buchi <devybuchi@gmail.com>
2022-09-16 06:03:06 -0300

    Update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,9 @@ function Footer() {
   const itsNot404Page = router.pathname !== '/404';
   return (
     (itsNot404Page && (
-      <footer className="h-fit w-full bg-primary_30 p-2 dark:bg-[#1a202d] ">
-        <div className="min-w[375px] flex max-w-[1945px] items-end justify-center">
-          <p className="p-3 text-center text-xs text-gray-300 ">
+      <footer className="h-fit w-full bg-primary_30 p-5 dark:bg-[#1a202d] ">
+        <div className="min-w[375px] flex max-w-[1945px] justify-center">
+          <p className="text-center text-[0.7rem] text-gray-300 ">
             &copy; 2019 - 2022 <span className="ml-2">Dylan Buchi</span>
           </p>
         </div>
