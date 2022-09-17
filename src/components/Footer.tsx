c4711b49363a6da8faf4cde8eb78f73960201943
Dylan Buchi <devybuchi@gmail.com>
2022-09-16 23:42:16 -0300

    Set state to add current year dynamically

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,13 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 function Footer() {
+  const [currentYear, setCurrentYear] = useState<string>();
   const router = useRouter();
+
+  useEffect(() => {
+    setCurrentYear(new Date().getFullYear().toString());
+  }, []);
 
   const itsNot404Page = router.pathname !== '/404';
   return (
@@ -9,7 +15,8 @@ function Footer() {
       <footer className="to-add-blur min-h-auto w-full bg-primary_30 p-5 dark:bg-[#1a202d] ">
         <div className="min-w[375px] flex max-w-[1945px] justify-center">
           <p className="text-center text-[0.7rem] text-gray-300 ">
-            &copy; 2019 - 2022 <span className="ml-2">Dylan Buchi</span>
+            &copy; 2019 - {currentYear}
+            <span className="ml-2">Dylan Buchi</span>
           </p>
         </div>
       </footer>
