b4821bdaa0a37869b7e8db4673766257abf8029d
Dylan Buchi <devybuchi@gmail.com>
2022-09-19 12:30:34 -0300

    Remove Container

--- src/containers/Container.tsx
+++
@@ -1,9 +0,0 @@
-import { Props } from 'interfaces/props';
-
-const Container = ({ children, className }: Props) => {
-  return (
-    <div className={`mb-auto overflow-y-scroll ${className}`}>{children}</div>
-  );
-};
-
-export default Container;
