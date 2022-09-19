38c3e46afeafe056c55b8098faf5537e93dbff77
Dylan Buchi <devybuchi@gmail.com>
2022-09-19 12:30:27 -0300

    Fix styles

--- src/containers/AppContainer.tsx
+++ src/containers/AppContainer.tsx
@@ -3,7 +3,7 @@ import { Props } from 'interfaces/props';
 const AppContainer = ({ children }: Props) => {
   return (
     <div
-      className={`full-height dark:text-white_gray" flex flex-col overflow-hidden bg-home bg-cover text-gray_10 dark:bg-primary_10 dark:bg-home_dark`}
+      className={`full-height flex flex-col bg-home bg-cover text-gray_10 dark:bg-primary_10 dark:bg-home_dark dark:text-gray-300`}
     >
       {children}
     </div>
