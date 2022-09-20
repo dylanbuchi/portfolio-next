1c9b21a9b266d2bf40a6036430e9b77c080f54ed
Dylan Buchi <devybuchi@gmail.com>
2022-09-20 12:21:47 -0300

    Update styles

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,10 +1,14 @@
 import MyHead from 'components/MyHead';
+import ProjectList from 'components/Projects/ProjectList';
 import type { NextPage } from 'next';
+import { PROJECTS_LIST } from '../constants/projects/projects';
 
 const ProjectsPage: NextPage = () => (
   <>
     <MyHead title="Projects" />
-    <div className="overflow-y-scroll"></div>
+    <div className="to-add-blur h-full overflow-y-auto">
+      <ProjectList projects={PROJECTS_LIST} />
+    </div>
   </>
 );
 
