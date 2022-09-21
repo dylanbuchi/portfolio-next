87b260a705e1f317a978b6ab30662aafb614a260
Dylan Buchi <devybuchi@gmail.com>
2022-09-21 15:37:12 -0300

    Add project gifs

---
+++ src/constants/images/python-31-days-code.ts
@@ -0,0 +1,28 @@
+import { PROJECT_IMAGE_SRC_BASE_URL } from 'constants/projects/projects';
+
+const wallpaperBot = `${PROJECT_IMAGE_SRC_BASE_URL}/106395737-93e5c600-63e2-11eb-9c9d-d799d2781809.gif`;
+
+const bestMovies = `${PROJECT_IMAGE_SRC_BASE_URL}/106367333-9d0e5e80-6320-11eb-8561-19702717a383.gif`;
+
+const flashCards = `${PROJECT_IMAGE_SRC_BASE_URL}/106188111-4fa9ba00-6185-11eb-903d-b251809fc54e.gif`;
+
+const pomodoroTimer = `${PROJECT_IMAGE_SRC_BASE_URL}/105880771-81cee680-5fe2-11eb-8adc-ff912239f1af.gif`;
+
+const crossyRoadGame = `${PROJECT_IMAGE_SRC_BASE_URL}/105533202-8afe4180-5cca-11eb-8754-e77d37dd618b.gif`;
+
+const pongGame = `${PROJECT_IMAGE_SRC_BASE_URL}/105402885-ba4b7a80-5c06-11eb-9e3c-111abe1a5e02.gif`;
+
+const snakeGame = `${PROJECT_IMAGE_SRC_BASE_URL}/105220061-f4991700-5b35-11eb-811b-b83e109ae762.gif`;
+
+const turtleGame = `${PROJECT_IMAGE_SRC_BASE_URL}/105069652-0492f700-5a61-11eb-8b47-5a97adc8a86c.gif`;
+
+export const GIFS = [
+  wallpaperBot,
+  bestMovies,
+  flashCards,
+  pomodoroTimer,
+  crossyRoadGame,
+  pongGame,
+  snakeGame,
+  turtleGame,
+];
