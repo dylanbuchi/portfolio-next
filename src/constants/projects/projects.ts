05804a3b4b19a4a868e11a620b9757b725fbe925
Dylan Buchi <devybuchi@gmail.com>
2022-09-21 01:00:45 -0300

    Refactor projects file and add tech icons

--- src/constants/projects/projects.ts
+++ src/constants/projects/projects.ts
@@ -1,6 +1,7 @@
-import type { ProjectType } from 'interfaces/projects';
+import type { ProjectTechs, ProjectType } from 'interfaces/projects';
 import { capitalize } from 'utilities/functions';
 import { v4 as uuidv4 } from 'uuid';
+import { icons } from '../../constants/images/icons';
 import { SOCIAL_LINKS, USERNAME } from '../socials';
 
 const getProjectName = (projectName: string) => {
@@ -10,6 +11,7 @@ const getProjectName = (projectName: string) => {
 const getProjectLink = (projectName: string) => {
   return `${SOCIAL_LINKS.github.website}/${USERNAME}/${projectName}`;
 };
+
 export const PROJECT_NAMES = {
   bigMovies: 'big-movies',
   francisPortfolio: 'francis-portfolio',
@@ -21,17 +23,17 @@ export const PROJECT_NAMES = {
   hangman: 'hangman',
 };
 
-const TECH_NAMES = {
-  python: 'Python',
-  django: 'Django',
-  css: 'Css',
-  javaScript: 'JavaScript',
-  react: 'React',
-  materialUI: 'Material UI',
-  flutter: 'Flutter',
-  dart: 'Dart',
-  java: 'Java',
-};
+const TECHS: ProjectTechs = {
+  python: { name: 'Python', icon: icons.python },
+  django: { name: 'Django', icon: icons.django },
+  css: { name: 'Css', icon: icons.css },
+  javaScript: { name: 'JavaScript', icon: icons.javaScript },
+  typeScript: { name: 'TypeScript', icon: icons.typeScript },
+  react: { name: 'React', icon: icons.react },
+  flutter: { name: 'Flutter', icon: icons.flutter },
+  dart: { name: 'Dart', icon: icons.dart },
+  java: { name: 'Java', icon: icons.java },
+} as const;
 
 const PROJECT_IMAGE_SRC_BASE_URL =
   'https://user-images.githubusercontent.com/52018183';
@@ -39,7 +41,7 @@ const PROJECT_IMAGE_SRC_BASE_URL =
 const PROJECTS: { [name: string]: ProjectType } = {
   [PROJECT_NAMES.bigMovies]: {
     name: PROJECT_NAMES.bigMovies,
-    techs: [TECH_NAMES.react, TECH_NAMES.javaScript, TECH_NAMES.materialUI],
+    techs: [TECHS.react, TECHS.javaScript, TECHS.css],
     imgSrc: `${PROJECT_IMAGE_SRC_BASE_URL}/176925671-87c2daa4-115f-4440-acc7-218e2146701d.png`,
     demoLink: 'https://big-movies.netlify.app/',
   },
@@ -47,45 +49,45 @@ const PROJECTS: { [name: string]: ProjectType } = {
   [PROJECT_NAMES.francisPortfolio]: {
     name: PROJECT_NAMES.francisPortfolio,
     alternativeName: "Francis's Portfolio",
-    techs: [TECH_NAMES.python],
+    techs: [TECHS.javaScript, TECHS.python, TECHS.css],
     imgSrc: `${PROJECT_IMAGE_SRC_BASE_URL}/97243778-d42b1c00-17d5-11eb-82b3-431cece272e1.png`,
     demoLink: 'https://fbrempong.pythonanywhere.com',
   },
 
   [PROJECT_NAMES.findCustomers]: {
     name: PROJECT_NAMES.findCustomers,
-    techs: [TECH_NAMES.django, TECH_NAMES.python, TECH_NAMES.css],
+    techs: [TECHS.django, TECHS.python, TECHS.css],
     imgSrc: `${PROJECT_IMAGE_SRC_BASE_URL}/107127881-ef5afc80-6897-11eb-8ff6-0bc4377c2bd5.gif`,
     demoLink: 'https://find-customers.herokuapp.com/',
   },
 
   [PROJECT_NAMES.python31days]: {
     name: PROJECT_NAMES.python31days,
-    techs: [TECH_NAMES.python],
+    techs: [TECHS.python],
     imgSrc: `${PROJECT_IMAGE_SRC_BASE_URL}/105533202-8afe4180-5cca-11eb-8754-e77d37dd618b.gif`,
   },
 
   [PROJECT_NAMES.discordBot]: {
     name: PROJECT_NAMES.discordBot,
-    techs: [TECH_NAMES.python],
+    techs: [TECHS.python],
     imgSrc: `${PROJECT_IMAGE_SRC_BASE_URL}/96628718-fe1da380-12e8-11eb-8787-4c7ff537a310.png`,
   },
 
   [PROJECT_NAMES.expenses]: {
     name: PROJECT_NAMES.expenses,
-    techs: [TECH_NAMES.flutter, TECH_NAMES.dart],
+    techs: [TECHS.flutter, TECHS.dart],
     imgSrc: `${PROJECT_IMAGE_SRC_BASE_URL}/62386638-d947df80-b52e-11e9-9745-a5de55649ebb.gif`,
   },
 
   [PROJECT_NAMES.movies]: {
     name: PROJECT_NAMES.movies,
-    techs: [TECH_NAMES.flutter, TECH_NAMES.dart],
+    techs: [TECHS.flutter, TECHS.dart],
     imgSrc: `${PROJECT_IMAGE_SRC_BASE_URL}/63264632-f20c0100-c261-11e9-8ff4-6ee8599ac964.png`,
   },
 
   [PROJECT_NAMES.hangman]: {
     name: PROJECT_NAMES.hangman,
-    techs: [TECH_NAMES.java],
+    techs: [TECHS.java],
     imgSrc: `${PROJECT_IMAGE_SRC_BASE_URL}/71909796-7a2a6e80-314f-11ea-8797-1fa7a6614545.png`,
   },
 };
