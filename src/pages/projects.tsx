339333c03fddbcadf945b172cb7e0b2977ad86f2
Dylan Buchi <devybuchi@gmail.com>
2022-09-21 16:24:19 -0300

    Set dynamic gif to project

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,15 +1,34 @@
 import MyHead from 'components/MyHead';
 import ProjectList from 'components/Projects/ProjectList';
+import { GIFS } from 'constants/images/python-31-days-code';
 import type { NextPage } from 'next';
+import { useState } from 'react';
 import { PROJECTS_LIST } from '../constants/projects/projects';
 
-const ProjectsPage: NextPage = () => (
-  <>
-    <MyHead title="Projects" />
-    <div className="to-add-blur h-full overflow-y-auto">
-      <ProjectList projects={PROJECTS_LIST} />
-    </div>
-  </>
-);
+const ProjectsPage: NextPage = () => {
+  const itemToSetGif = '31-days-of-code';
+
+  const [projects, setProjects] = useState(() => {
+    const index = Math.floor(Math.random() * GIFS.length);
+    const gif = GIFS[index];
+
+    const projects = PROJECTS_LIST.map((item) => {
+      if (itemToSetGif === item.projectGithubName) {
+        return { ...item, imgSrc: gif };
+      }
+      return item;
+    });
+    return projects;
+  });
+
+  return (
+    <>
+      <MyHead title="Projects" />
+      <div className="to-add-blur h-full overflow-y-auto">
+        <ProjectList projects={projects} />
+      </div>
+    </>
+  );
+};
 
 export default ProjectsPage;
