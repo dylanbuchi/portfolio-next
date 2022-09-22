06676c6fbc1b8d13b838a9fbef8f11c8d9310c7d
Dylan Buchi <devybuchi@gmail.com>
2022-09-22 04:23:33 -0300

    Add image og meta tag

--- src/components/MyHead.tsx
+++ src/components/MyHead.tsx
@@ -3,9 +3,17 @@ import Head from 'next/head';
 interface MyHeadProps {
   title: string;
   description?: string;
+  imageSrc?: string;
 }
 
-const MyHead = ({ title, description }: MyHeadProps) => {
+const DEFAULT_IMG_SRC =
+  'https://user-images.githubusercontent.com/52018183/97459431-89ff8300-191a-11eb-93c2-36a04db97006.png';
+
+const MyHead = ({
+  title,
+  description,
+  imageSrc = DEFAULT_IMG_SRC,
+}: MyHeadProps) => {
   return (
     <Head>
       <meta name="title" property="og:title" content={title} />
@@ -14,6 +22,9 @@ const MyHead = ({ title, description }: MyHeadProps) => {
         property="og:description"
         content={description}
       />
+      <meta property="og:image" content={imageSrc} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       <title>{title}</title>
     </Head>
   );
