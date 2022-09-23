56a01590c4543ecf23e20976f5b649509892011b
Dylan Buchi <devybuchi@gmail.com>
2022-09-23 17:16:11 -0300

    Add useEffect to change gif on initial render

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { GIFS } from 'constants/images/python-31-days-code';
 import { PROJECTS_LIST } from 'constants/projects/projects';
@@ -10,18 +10,21 @@ import ProjectList from 'components/Projects/ProjectList';
 const ProjectsPage: NextPage = () => {
   const itemToSetGif = '31-days-of-code';
 
-  const [projects, setProjects] = useState(() => {
+  const [projects, setProjects] = useState(() => PROJECTS_LIST);
+
+  useEffect(() => {
     const index = Math.floor(Math.random() * GIFS.length);
     const gif = GIFS[index];
 
-    const projects = PROJECTS_LIST.map((item) => {
-      if (itemToSetGif === item.projectGithubName) {
-        return { ...item, imgSrc: gif };
-      }
-      return item;
-    });
-    return projects;
-  });
+    setProjects((prev) =>
+      prev.map((item) => {
+        if (itemToSetGif === item.projectGithubName) {
+          return { ...item, imgSrc: gif };
+        }
+        return item;
+      }),
+    );
+  }, []);
 
   return (
     <>
