e805c4f1df456a66f7aeed02fb4d0eb8fbeefc54
Dylan Buchi <devybuchi@gmail.com>
2022-09-24 06:02:31 -0300

    Add emailChecker function

--- src/utilities/functions.ts
+++ src/utilities/functions.ts
@@ -20,4 +20,27 @@ export const capitalize = (word: string) => {
   return letters.join('');
 };
 
+export const emailChecker = (email: string) => {
+  if (!email) return false;
+
+  const valid_email_suffix = email.endsWith('.com');
+  const valid_at_sign =
+    email.split('').filter((item) => item === '@').length === 1;
+
+  const validFirstChar = email[0].match(/[a-z0-9]/gi);
+
+  if (!valid_at_sign || !valid_email_suffix || !validFirstChar) return false;
+
+  const domain = email.split('@')[1];
+
+  let dot_count = 0;
+
+  for (const char of domain) {
+    if (dot_count > 1) return false;
+    if (char === '.') dot_count++;
+  }
+
+  return true;
+};
+
 export default setClassNames;
