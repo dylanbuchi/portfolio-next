b1b70a16f6b6d1b504f4c1943ba41ef3b389f89f
Dylan Buchi <devybuchi@gmail.com>
2022-09-24 00:39:34 -0300

    Add form endpoint env

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     unoptimized: true,
     domains: ['user-images.githubusercontent.com'],
   },
+  env: {
+    getFormIoEndpoint: process.env.YOUR_FORM_END_POINT,
+  },
 };
 
 module.exports = nextConfig;
