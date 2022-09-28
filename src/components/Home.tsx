24d170335a8b934aff0ac62de40545d2a5e2aa26
Dylan Buchi <devybuchi@gmail.com>
2022-09-28 13:39:32 -0300

    Add icon names to Home page icons

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,7 +8,7 @@ import SocialLink from './SocialLink';
 const Home = () => {
   const helloMsg = "Hi, I'm ";
   const deviconBoxStyles =
-    'hover:lumos relative ml-4 h-9 w-9 shrink-0 lg:h-10 lg:w-10 h-9 w-9';
+    'home-icons-after hover:lumos relative ml-4 h-9 w-9 shrink-0 lg:h-10 lg:w-10 h-9 w-9   md:after:left-[160%]';
 
   const iconStyle =
     'ml-[0.1rem] cursor-pointer rounded-xl bg-[#dae6f2] p-3 text-3xl shadow-sm shadow-primary_10 transition hover:scale-150 hover:bg-transparent hover:shadow-none dark:bg-[#254d77] dark:shadow-none dark:hover:bg-transparent';
@@ -55,21 +55,27 @@ const Home = () => {
       <p className="text-md mt-auto px-2 text-center font-medium sm:ml-0 ">
         Built with:
       </p>
-      <div className="w-full bg-gradient-to-r from-[#b3cad5] to-primary_20 shadow-sm shadow-primary_10 dark:bg-transparent dark:bg-gradient-to-r dark:from-primary_20 dark:to-primary_10">
+      <div className="relative w-full bg-gradient-to-l from-[#63869a] to-primary_30 shadow-sm shadow-primary_10 dark:bg-transparent dark:bg-gradient-to-r dark:from-primary_20 dark:to-primary_10">
         <div className="flex w-full items-center justify-center p-6 dark:shadow-none landscape_max:flex landscape_max:justify-center">
-          <div className="flex w-[100%] items-center justify-evenly sm:w-[65%]">
+          <div className="flex w-[100%] items-center justify-evenly sm:w-[65%] md:w-[90%] lg:w-[75%] lg:justify-between">
             <div
-              className={`${deviconBoxStyles} ml-0 rounded-full bg-white_gray text-center`}
+              className={`${deviconBoxStyles} ml-0 rounded-full bg-white_gray text-center after:left-[40%] after:content-["NextJs"]`}
             >
               {<Devicon name="nextjs" type="line" />}
             </div>
-            <div className={`${deviconBoxStyles}dark:bg-transparent`}>
+            <div
+              className={`${deviconBoxStyles} after:left-[45%] after:content-["React"] dark:bg-transparent`}
+            >
               {<Devicon name="react" />}
             </div>
-            <div className={`${deviconBoxStyles}`}>
+            <div
+              className={`${deviconBoxStyles} after:left-[12%] after:content-["TypeScript"] `}
+            >
               {<Devicon name="typescript" />}
             </div>
-            <div className={`${deviconBoxStyles}`}>
+            <div
+              className={`${deviconBoxStyles} after:left-[%] after:content-["TailwindCss"] md:mr-20  lg:mr-24`}
+            >
               {<Devicon name="tailwindcss" type="plain" />}
             </div>
           </div>
