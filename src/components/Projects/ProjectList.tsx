e077b40cd2eaf22cc9f1550ceb8efc62d01426f0
Dylan Buchi <devybuchi@gmail.com>
2022-09-29 13:22:55 -0300

    Update projects grid for large screens

--- src/components/Projects/ProjectList.tsx
+++ src/components/Projects/ProjectList.tsx
@@ -7,8 +7,8 @@ interface ProjectListProps {
 
 const ProjectList = ({ projects }: ProjectListProps) => {
   return (
-    <div className=" mx-10 mb-10 py-10 lg:m-0 lg:p-0 landscape_max:p-0">
-      <ul className="xl:col-5 align grid grid-cols-1 gap-16 sm:grid-cols-2 sm:gap-x-6 md:pb-20 lg:-mt-9 lg:grid-cols-4 lg:items-end lg:justify-items-center lg:gap-0 lg:pb-0 xl:gap-x-8 landscape_max:gap-0 landscape_max:p-0">
+    <div className="mx-10 mb-10 py-10 lg:m-0 lg:p-0 landscape_max:p-0">
+      <ul className="xl:col-5 align grid grid-cols-1 gap-16 sm:grid-cols-2 sm:gap-x-6 md:pb-20 lg:-mt-7 lg:grid-cols-4 lg:grid-rows-[440px_440px_1fr] lg:place-items-end lg:justify-items-center lg:gap-0  lg:pb-0 xl:gap-x-8 landscape_max:gap-0 landscape_max:p-0">
         {projects.map((item) => (
           <Project key={item.id} project={item} />
         ))}
