854e8e7db17d18ef5d4aef2713c4ddcab089304f
Dylan Buchi <devybuchi@gmail.com>
2022-10-01 08:48:27 -0300

    Remove MyHead from _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,13 @@
 import type { AppProps } from 'next/app';
-
 import Layout from 'layouts/Layout';
 import useIsComponentLoaded from 'utilities/hooks/useIsComponentLoaded';
 
 import 'styles/main.css';
-import MyHead from 'components/MyHead';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     useIsComponentLoaded() && (
       <>
-        <MyHead />
         <div
           className={`full-height flex flex-col bg-home bg-cover text-gray_10 dark:bg-primary_10 dark:bg-home_dark dark:text-gray-300`}
         >
